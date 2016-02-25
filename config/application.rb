9f39142460f56f418e64445f04537a98c4e92e31
kenken5232 <zwei_bird@hotmail.com>
2016-02-25 09:31:43 +0000

    use bootstrap

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,9 @@ module Workspace
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # to auto load lib/ directory
+    #config.autoload_paths += %W(#{config.root}/lib)
+    config.autoload_paths += %W(#{config.root}/lib #{config.root}/lib)
   end
 end
